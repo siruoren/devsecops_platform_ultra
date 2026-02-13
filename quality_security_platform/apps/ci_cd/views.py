d4abbdd63ee6dead1e25848fdb66891f2fb3102d
siruoren <zhuguojun6@163.com>
2026-02-13 11:51:25 +0800

    update

--- quality_security_platform/apps/ci_cd/views.py
+++ quality_security_platform/apps/ci_cd/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status, , filters
+from rest_framework import viewsets, status, filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.utils import timezone
